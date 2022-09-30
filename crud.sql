ef7c54ad9622797322ad7660d418db7d92f5e493
Bruno Azevedo <93809602+brunoacev@users.noreply.github.com>
2022-09-30 12:49:32 -0300

    Update Final Entrega SQLanches

--- crud.sql
+++ crud.sql
@@ -4,31 +4,55 @@
 -- Criação
 
 -- 1)
-
+INSERT INTO clientes (nome, lealdade)
+VALUES ('Georgia', 0);
 
 
 -- 2)
-
+INSERT INTO pedidos (status, cliente_id)
+VALUES('Recebido', (SELECT id FROM clientes WHERE nome = 'Georgia'));
 
 
 -- 3)
-
+INSERT INTO produtos_pedidos(pedido_id, produto_id)
+VALUES
+  (6, 1),
+  (6, 2),
+  (6, 6),
+  (6, 8),
+  (6, 8);
 
 
 -- Leitura
 
 -- 1)
-
+SELECT 
+  cl.id, cl.nome, cl.lealdade, p.id, p.status, p.cliente_id,
+  pr.id, pr.nome, pr.tipo, pr."preço", pr.pts_de_lealdade
+FROM
+  clientes cl
+JOIN
+  pedidos p ON cl.id = p.cliente_id
+JOIN
+  produtos_pedidos pp ON p.id = pp.pedido_id
+JOIN
+  produtos pr ON pp.produto_id = pr.id
+WHERE cl.nome = 'Georgia';
 
 
 -- Atualização
 
 -- 1)
+UPDATE clientes cl SET lealdade = 48
+WHERE cl.nome = 'Georgia';
 
 
 
 -- Deleção
 
 -- 1)
-
+DELETE FROM
+  Clientes cl
+WHERE
+  cl.nome = 'Marcelo' RETURNING *;
 
--- selects.sql
+++ selects.sql
@@ -3,10 +3,48 @@
 
 -- 1)
 
+SELECT
+p.id, p.status, p.cliente_id, pr.id, pr.nome, pr.tipo,
+pr."preço", pr.pts_de_lealdade pontos_de_lealdade_do_produto 
+FROM pedidos p
+JOIN produtos_pedidos pp ON p.id = pp.pedido_id
+JOIN produtos pr ON pr.id = pp.produto_id;
+
 -- 2)
+
+SELECT 
+  p.id
+FROM pedidos p
+JOIN produtos_pedidos pp ON pp.pedido_id = p.id
+JOIN produtos pr ON pp.produto_id = pr.id
+WHERE pr.nome ILIKE('%fritas%');
 
 -- 3)
 
+SELECT
+  cl.nome gostam_de_fritas
+FROM clientes cl
+JOIN pedidos p ON cl.id = p.cliente_id
+JOIN produtos_pedidos pp ON p.id = pp.pedido_id
+JOIN produtos pr ON pp.produto_id = pr.id
+WHERE pr.nome ILIKE('%fritas%');
 -- 4)
 
+SELECT
+  SUM(pr."preço")
+FROM
+  produtos pr
+JOIN produtos_pedidos pp ON pr.id = pp.produto_id
+JOIN pedidos p ON pp.pedido_id = p.id
+JOIN clientes cl ON p.cliente_id = cl.id
+WHERE cl.nome = 'Laura';
 -- 5)
+
+SELECT
+  pr.nome, COUNT(p)
+FROM
+  produtos pr
+JOIN produtos_pedidos pp ON pr.id = pp.produto_id
+JOIN pedidos p ON pp.pedido_id = p.id
+GROUP BY pr.nome
+ORDER BY pr.nome ASC;
